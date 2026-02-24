38d044bbb65fdbc955fb060b49a9640acf56e5e4
jacobhaileyebio <@Jacob.Yebio>
2026-02-24 14:56:52 +0100

    Added API and handler for getBalance. Also made a few changes to runBankServer to make it more readable

--- src/APIs.hs
+++ src/APIs.hs
@@ -15,6 +15,7 @@ import Network.HTTP.Client (defaultManagerSettings, newManager)
 
 import Control.Concurrent (forkIO, killThread, threadDelay)
 import Control.Exception (bracket)
+import Control.Monad.Freer.Reader
 import Control.Monad.IO.Class
 import Data.Map (Map)
 import Data.Map qualified as Map
@@ -45,6 +46,8 @@ type CapabilityAPI =
 
 type BankAPI =
   "getAccountId" :> ReqBody '[JSON] (Capability AccountCapability) :> Get '[JSON] AccountId
+    :<|> "getBalance" :> ReqBody '[JSON] (Capability AccountCapability) :> Get '[JSON] Amount
+    :<|> "createTestAccount" :> Get '[JSON] (Capability AccountCapability)
 
 bankapi :: Proxy BankAPI
 bankapi = Proxy
@@ -63,16 +66,29 @@ server bankRef capability =
     :<|> getBalance bankRef capability
     :<|> getTransfer bankRef capability
 -}
-type BankM = Eff (CapabilityEffect '[IO] AccountCapability ': IO ': '[])
+type BankM = Eff (Reader (TVar (Map AccountId Amount)) ': CapabilityEffect '[IO] AccountCapability ': IO ': '[])
 
 bankServer :: ServerT BankAPI BankM
-bankServer = handleGetAccountID
+bankServer =
+  handleGetAccountID
+    :<|> handleGetBalance
+    :<|> handleCreateTestAccount
 
 handleGetAccountID :: Capability AccountCapability -> BankM AccountId
 handleGetAccountID account = use @'[IO] account GetAccountId
 
-bankApp :: TVar (CapabilityMap '[IO] AccountCapability) -> Application
-bankApp s = serve bankapi $ hoistServer bankapi (liftIO . runM . runCapabilityEffectSTM' s) bankServer
+handleGetBalance :: Capability AccountCapability -> BankM Amount
+handleGetBalance account = use @'[IO] account GetBalance
+
+handleCreateTestAccount :: BankM (Capability AccountCapability)
+handleCreateTestAccount = do
+  bankRef <- ask
+  bank <- send $ readTVarIO bankRef
+  let AccountId maxId = maximum (Map.keys bank)
+  raise $ createAccount bankRef (AccountId (maxId + 1))
+
+bankApp :: TVar (Map AccountId Amount) -> TVar (CapabilityMap '[IO] AccountCapability) -> Application
+bankApp bank s = serve bankapi $ hoistServer bankapi (liftIO . runM . runCapabilityEffectSTM' s . runReader bank) bankServer
 
 runBankServer :: IO ()
 runBankServer = do
@@ -80,28 +96,17 @@ runBankServer = do
   mgr <- newManager defaultManagerSettings
   capMap <- newTVarIO emptyCapabilityMap
   readTVarIO capMap >>= print
-  account <- runM $ runCapabilityEffectSTM' capMap $ do
-    bankRef <- send $ newTVarIO Map.empty
-    let id0 = AccountId 0
-    createAccount bankRef id0
+  bankRef <- newTVarIO Map.empty
   readTVarIO capMap >>= print
-  let runApp = run port (bankApp capMap)
+  let runApp = run port (bankApp bankRef capMap)
   bracket (forkIO runApp) killThread $ \_ -> do
-    let getAccountId' = client bankapi
+    let getAccountId' :<|> getBalance' :<|> createTestAccount = client bankapi
     _ <- flip runClientM (mkClientEnv mgr (BaseUrl Http "localhost" 8080 "")) $ do
-      idT <- getAccountId' account
+      testAccount <- createTestAccount
+      idT <- getAccountId' testAccount
       liftIO $ print idT
+      bT <- getBalance' testAccount
+      liftIO $ print bT
       return ()
     threadDelay 3000000
     return ()
-
--- Skal gjøre det samme som createAccount i BankCapability
-{-
-
-getBalance :: TVar BankState -> Capability AccountCapability -> AccountCapability Amount -> Handler Amount
-getBalance _bankRef _cap _balanceCap = undefined
-
-getTransfer :: TVar BankState -> Capability AccountCapability -> AccountCapability (Either String Amount) -> Handler (Either String Amount)
-getTransfer _bankRef _cap _transferCap = undefined
-
--}
