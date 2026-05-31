5fc009dff7e8c300b63aa3c5a8e5bee5e98a25d0
jacobhaileyebio <@Jacob.Yebio>
2026-05-31 22:57:15 +0200

    changed output message in mailbox example

--- src/ContactCapability.hs
+++ src/ContactCapability.hs
@@ -1,94 +1,99 @@
-{-# LANGUAGE GADTs, DataKinds, DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module ContactCapability where 
+module ContactCapability where
 
-
-import GHC.Generics (Generic)
+import Control.Concurrent.STM ()
+import Control.Monad
+import Control.Monad.Freer
 import Data.Aeson
-import Control.Monad.Freer 
-import Control.Concurrent.STM()  
-import Control.Monad 
-import FreerCapability
-  ( CapabilityEffect
-  , Capability
-  , create
-  , use
-  , CapabilityMap(CapabilityMap) 
-  )
-
+import FreerCapability (
+  Capability,
+  CapabilityEffect,
+  CapabilityMap (CapabilityMap),
+  create,
+  use,
+ )
+import GHC.Generics (Generic)
 
 data ContactCapability a where
   Name :: ContactCapability String
   SendMessage :: String -> ContactCapability ()
-  
+
 instance Show (ContactCapability a) where
   show Name = "Name"
-  show (SendMessage msg) = "SendMessage " ++ show msg  
+  show (SendMessage msg) = "SendMessage " ++ show msg
 
-instance ToJSON (ContactCapability a) where 
-    toJSON Name = object ["action" .=  ("Name" :: String)] 
-    toJSON (SendMessage msg) = object ["action" .= ("SendMessage" :: String) , "message" .= msg] 
-  
+instance ToJSON (ContactCapability a) where
+  toJSON Name = object ["action" .= ("Name" :: String)]
+  toJSON (SendMessage msg) = object ["action" .= ("SendMessage" :: String), "message" .= msg]
 
-instance FromJSON (ContactCapability String) where 
-    parseJSON = withObject "ContactCapability" $ \v -> Name <$ (do 
-                                                                  x <- v.: "action" 
-                                                                  guard $ x  == ("Name" :: String))
+instance FromJSON (ContactCapability String) where
+  parseJSON = withObject "ContactCapability" $ \v ->
+    Name
+      <$ ( do
+             x <- v .: "action"
+             guard $ x == ("Name" :: String)
+         )
 
+instance FromJSON (ContactCapability ()) where
+  parseJSON = withObject "ContactCapability" $ \v ->
+    SendMessage
+      <$> ( do
+              x <- v .: "action"
+              guard $ x == ("SendMessage" :: String)
+              v .: "message"
+          )
 
-instance FromJSON (ContactCapability ()) where 
-    parseJSON = withObject "ContactCapability" $ \v -> SendMessage  <$> (do 
-                                                                  x <- v.: "action" 
-                                                                  guard $ x  == ("SendMessage" :: String)
-                                                                  v.: "message")
-
-data ContactRequest 
-  = GetNameRequest Integer 
-  | SendMessageRequest Integer String 
+data ContactRequest
+  = GetNameRequest Integer
+  | SendMessageRequest Integer String
   deriving (Show, Generic)
 
-instance ToJSON ContactRequest 
-instance FromJSON ContactRequest 
+instance ToJSON ContactRequest
+instance FromJSON ContactRequest
 
 data ContactResponse
-  = NameResponse String  
-  | MessageSentResponse 
-  | ErrorMessageResponse String 
-  deriving (Show, Generic) 
+  = NameResponse String
+  | MessageSentResponse
+  | ErrorMessageResponse String
+  deriving (Show, Generic)
 
-instance ToJSON ContactResponse 
-instance FromJSON ContactResponse  
+instance ToJSON ContactResponse
+instance FromJSON ContactResponse
 
-
-{- There wil be a similar function called createBankAccount in BankCapability.hs. 
- - The bank needs to store the bank accounts. Try to use STM TVar and map. 
+{- There wil be a similar function called createBankAccount in BankCapability.hs.
+ - The bank needs to store the bank accounts. Try to use STM TVar and map.
  - Just like there is a Name and a SendMessage there needs to be one for eacth BankCapability
  - -}
 createMailBox :: String -> Eff (CapabilityEffect '[IO] ContactCapability ': IO ': '[]) (Capability ContactCapability)
-createMailBox name = create @'[IO] (\c -> case c of 
-    Name -> pure name  
-    (SendMessage msg) -> send $ putStrLn msg )
+createMailBox name =
+  create @'[IO]
+    ( \c -> case c of
+        Name -> pure name
+        (SendMessage msg) -> send $ putStrLn msg
+    )
 
 initialMap :: CapabilityMap effs f
 initialMap = CapabilityMap (0, \_ -> error "Capability not found")
 
-type ContactTest = CapabilityEffect '[IO] ContactCapability ': IO ': '[]  
+type ContactTest = CapabilityEffect '[IO] ContactCapability ': IO ': '[]
 
 data TestEffect a where
-    GetInt :: TestEffect Int
-    GetString :: TestEffect String
-
+  GetInt :: TestEffect Int
+  GetString :: TestEffect String
 
 simpleHandler :: TestEffect a -> a
 simpleHandler GetInt = 42
-simpleHandler GetString = "hello"
+simpleHandler GetString = "Hello"
 
-
-mailDeliveryTest :: Eff ContactTest () 
-mailDeliveryTest = do 
-    m <- createMailBox "jacob"
-    use @'[IO] m $ SendMessage "Hallo"
-
+mailDeliveryTest :: Eff ContactTest ()
+mailDeliveryTest = do
+  m <- createMailBox "General Kenobi!"
+  name <- use @'[IO] m Name
+  use @'[IO] m $ SendMessage $ "Hello there. " ++ name
 
 testModulesWork :: IO ()
 testModulesWork = do
